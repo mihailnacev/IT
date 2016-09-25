3d44757e2b58f497a935167e0919690c2ed6cb70
mihailnacev <m.nacev@yahoo.com>
2016-09-26 00:57:20 +0200

    Small changes v2

--- WebApplication1/Repertoar.aspx.designer.cs
+++ WebApplication1/Repertoar.aspx.designer.cs
@@ -183,6 +183,15 @@ public partial class Repertoar {
     protected global::System.Web.UI.WebControls.DetailsView dvPretstavi;
     
     /// <summary>
+    /// lbStat control.
+    /// </summary>
+    /// <remarks>
+    /// Auto-generated field.
+    /// To modify move field declaration from designer file to code-behind file.
+    /// </remarks>
+    protected global::System.Web.UI.WebControls.Label lbStat;
+    
+    /// <summary>
     /// btnBackView3 control.
     /// </summary>
     /// <remarks>
