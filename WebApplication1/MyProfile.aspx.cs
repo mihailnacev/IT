14abab01c782de0b2322f3435f885f3cc40ccece
mihailnacev <m.nacev@yahoo.com>
2016-09-28 23:26:46 +0200

    Update MyProfile.aspx.cs

--- WebApplication1/MyProfile.aspx.cs
+++ WebApplication1/MyProfile.aspx.cs
@@ -79,7 +79,7 @@ public partial class MyProfile : System.Web.UI.Page
             {
                 //ListItem li = new ListItem(citac[0].ToString() + " " + citac[2].ToString(), citac[0].ToString());
                 //lbRezervacii.Items.Add(li);
-                pretstavi1.Add(citac[0].ToString() + " - " + citac[2].ToString());
+                pretstavi1.Add(citac[0].ToString() + "-" + citac[2].ToString());
                 pretstavi2.Add(citac[0].ToString());
             }
             lbRezervacii.DataSource = pretstavi1;
