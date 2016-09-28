74550f0b7133c1c945fe47f05968050c3e6f5f03
Martina <martina.tosevska.95@gmail.com>
2016-09-28 20:14:41 +0200

    Functionality in carousel

--- WebApplication1/Scripts/ScriptPocetna.js
+++ WebApplication1/Scripts/ScriptPocetna.js
@@ -2,14 +2,14 @@
     $("#SiteMap").hide();
 
     $("#skrienoKopche").hide();
-    
-        $("#skrienTextBox").hide();
-    
-        $(".carouselButton").click(function () {
-            $("#skrienTextBox").val($(this).attr("title"));
-            event.preventDefault();
-            $("#skrienoKopche").trigger("click");
-        })
+
+    $("#skrienTextBox").hide();
+
+    $(".carouselButton").click(function () {
+        $("#skrienTextBox").val($(this).attr("title"));
+        event.preventDefault();
+        $("#skrienoKopche").trigger("click");
+    })
 })
 
 $('.carousel').carousel();
