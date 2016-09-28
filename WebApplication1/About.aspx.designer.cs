0ebad3542f85350151a725c93d213919e3e04ae5
mihailnacev <m.nacev@yahoo.com>
2016-09-28 22:00:05 +0200

    Finally

--- WebApplication1/About.aspx.designer.cs
+++ WebApplication1/About.aspx.designer.cs
@@ -192,6 +192,15 @@ public partial class About {
     protected global::System.Web.UI.WebControls.Label label9;
     
     /// <summary>
+    /// Image11 control.
+    /// </summary>
+    /// <remarks>
+    /// Auto-generated field.
+    /// To modify move field declaration from designer file to code-behind file.
+    /// </remarks>
+    protected global::System.Web.UI.WebControls.Image Image11;
+    
+    /// <summary>
     /// label10 control.
     /// </summary>
     /// <remarks>
