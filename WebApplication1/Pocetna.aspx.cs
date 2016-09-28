0dc7270b9ed5260fdac5ecf5a5b4db75799d13c6
Martina <martina.tosevska.95@gmail.com>
2016-09-28 20:11:00 +0200

    Functionality in carousel

--- WebApplication1/Pocetna.aspx.cs
+++ WebApplication1/Pocetna.aspx.cs
@@ -97,9 +97,15 @@ public partial class Pocetna : System.Web.UI.Page
     protected void Button1_Click(object sender, EventArgs e)
     {
         
-        if(Session["button1Clicked"]==null)
-        Session["button1Clicked"] = 1;
-        Response.Redirect("~/Repertoar.aspx");
+        //if(Session["button1Clicked"]==null)
+        //Session["button1Clicked"] = 1;
+        Response.Redirect("~/Repertoar.aspx?search=true");
 
+    }
+
+    protected void skrienoKopche_Click(object sender, EventArgs e)
+    {
+        getInformations(skrienTextBox.Text);
+        Response.Redirect("~/PretstavaDetails.aspx");
     }
 }
--- WebApplication1/Pocetna.aspx.designer.cs
+++ WebApplication1/Pocetna.aspx.designer.cs
@@ -21,6 +21,24 @@ public partial class Pocetna {
     protected global::System.Web.UI.WebControls.Panel Main;
     
     /// <summary>
+    /// skrienoKopche control.
+    /// </summary>
+    /// <remarks>
+    /// Auto-generated field.
+    /// To modify move field declaration from designer file to code-behind file.
+    /// </remarks>
+    protected global::System.Web.UI.WebControls.Button skrienoKopche;
+    
+    /// <summary>
+    /// skrienTextBox control.
+    /// </summary>
+    /// <remarks>
+    /// Auto-generated field.
+    /// To modify move field declaration from designer file to code-behind file.
+    /// </remarks>
+    protected global::System.Web.UI.WebControls.TextBox skrienTextBox;
+    
+    /// <summary>
     /// ScriptManager1 control.
     /// </summary>
     /// <remarks>
